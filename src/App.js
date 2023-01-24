abb04b0cb962a7a61480081c87184a542b283e61
yinhang_sun <sunyinhang@gmail.com>
2023-01-24 14:52:51 -0800

    Creating Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-
 import './App.css';
+import { Component } from 'react';
 
 function App() {
   return (
     <div className="App">
-      My Todo Application Updated
+      <FirstComponent></FirstComponent>
+      <SecondComponent></SecondComponent>
+      <ThirdComponent></ThirdComponent>
+      <FourComponent></FourComponent>
+      
     </div>
   );
+}
+
+function FirstComponent() {
+  return (
+    <div className='FirstComponent'>First Component</div>
+    
+  )
+}
+
+function SecondComponent() {
+  return (
+    <div className='SecondComponent'>Second Component</div>
+  )
+}
+
+class ThirdComponent extends Component{
+  render() {
+    return (
+      <div className='ThirdComponent'>Third Component</div>
+    )
+  }
+}
+
+class FourComponent extends Component {
+  render() {
+    return (
+      <div className='FourComponent'>Four Component</div>
+    )
+  }
 }
 
 export default App;
