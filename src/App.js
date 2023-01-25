1ab49d5289b9f8cbf2c50cb65e788c440573c4f4
yinhang_sun <sunyinhang@gmail.com>
2023-01-24 16:52:03 -0800

    Getting Started with React Application - Counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import LearningComponent from './components/learning-examples/LearningComponent'
+import Counter from './components/counter/Counter';
 
 function App() {
   return (
     <div className="App">
-      <LearningComponent />
+      <Counter />
     </div>
   )
 }
