215fa9cabf1ece1a968d5f9bfefff702e0c7362e
yinhang_sun <sunyinhang@gmail.com>
2023-01-26 15:29:03 -0800

    Using Bootstrap to Style Todo React Front End Application

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -6,8 +6,9 @@ import './TodoApp.css'
 export default function TodoApp() {
     return (
         <div className="TodoApp">
-            <HeaderComponent />
+            
             <BrowserRouter>
+                <HeaderComponent />
                 <Routes>
                     <Route path='/' element={<LoginComponent />}/>
                     <Route path='/login' element={<LoginComponent />}/>
@@ -17,8 +18,9 @@ export default function TodoApp() {
 
                     <Route path='*' element={<ErrorComponent />}/>
                 </Routes>
+                <FooterComponent />
             </BrowserRouter>
-            <FooterComponent />
+            
         </div>
     )
 }
@@ -155,21 +157,38 @@ function ListTodosComponent() {
 
 function HeaderComponent() {
     return (
-        <div className="header">
-            <div> 
-                Header <hr/>
+
+        <header className="border-bottom border-light border-5 mb-5 p-2">
+        <div className="container">
+            <div className="row">
+                <nav className="navbar navbar-expand-lg">
+                    <a className="navbar-brand ms-2 fs-2 fw-bold text-black" href="https://www.yinhang.com">Yinhang</a>
+                    <div className="collapse navbar-collapse">
+                        <ul className="navbar-nav">
+                            <li className="nav-item fs-5"><Link className="nav-link" to="/welcome/yinhang">Home</Link></li>
+                            <li className="nav-item fs-5"><Link className="nav-link" to="/todos">Todos</Link></li>
+                        </ul>
+                    </div>
+                    <ul className="navbar-nav">
+                        <li className="nav-item fs-5"><Link className="nav-link" to="/login">Login</Link></li>
+                        <li className="nav-item fs-5"><Link className="nav-link" to="/logout">Logout</Link></li>
+                    </ul>
+                </nav>
             </div>
         </div>
+    </header>
+
+
     )
 }
 
 function FooterComponent() {
     return (
-        <div className="footer">
-            <div> 
-                <hr/> Header 
+        <footer className="footer">
+            <div className="container"> 
+                Your Footer 
             </div>
-        </div>
+        </footer>
     )
 }
 
