eea41b416cf6b57384a8c74744e24c56a2f37978
yinhang_sun <sunyinhang@gmail.com>
2023-01-27 15:53:56 -0800

    Displaying Todos from Spring Boot REST API in React App

--- src/components/todo/ListTodosComponent.jsx
+++ src/components/todo/ListTodosComponent.jsx
@@ -1,13 +1,25 @@
+import { useEffect, useState } from "react";
+import { retrieveAllTodosForUsername } from "./api/TodoApiService";
+
 function ListTodosComponent() {
 
     const today = new Date();
     const targetDate = new Date(today.getFullYear() + 12, today.getMonth(), today.getDay());
 
-    const todos = [
-                    {id: 1, description: 'Learn AWS', done: false, targetDate: targetDate}, 
-                    {id: 2, description: 'Learn Full Stack Dev', done: false, targetDate: targetDate}, 
-                    {id: 3, description: 'Learn DevOps', done: false, targetDate: targetDate}
-                ]
+    const [todos, setTodos] = useState([])
+
+    useEffect ( () => refreshTodos(), [] )
+
+    function refreshTodos() {
+
+        retrieveAllTodosForUsername('Yinhang')
+        .then(response => {
+            setTodos(response.data)
+        })
+        .catch(error => console.log(error))
+
+    }
+
     return (
         <div className="container">
             <h1>Things You Want To Do!</h1>
@@ -30,7 +42,8 @@ function ListTodosComponent() {
                                     <td>{todo.id}</td>
                                     <td>{todo.description}</td>
                                     <td>{todo.done.toString()}</td>
-                                    <td>{todo.targetDate.toDateString()}</td>
+                                    {/* <td>{todo.targetDate.toDateString()}</td> */}
+                                    <td>{todo.targetDate.toString()}</td>
                                 </tr>
                             )
                         )
